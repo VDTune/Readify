c26e51c06b14705ebe670fd4801de37ee40e2f14
VDTune <voductuan1305@gmail.com>
2023-04-12 10:18:11 +0700

    category commit

---
+++ app/src/main/java/com/example/readify/AdapterCategory.kt
@@ -0,0 +1,86 @@
+package com.example.readify
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.readify.databinding.ActivityCategoryAddBinding
+import com.example.readify.databinding.RowCategoryBinding
+import com.google.firebase.database.FirebaseDatabase
+
+class AdapterCategory: RecyclerView.Adapter<AdapterCategory.HolderCategory> {
+    private val context: Context
+    private val categoryArrayList: ArrayList<ModelCategory>
+    private lateinit var binding: RowCategoryBinding
+    // constructor
+    constructor(context: Context, categoryArrayList: ArrayList<ModelCategory>) {
+        this.context = context
+        this.categoryArrayList = categoryArrayList
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderCategory {
+        binding = RowCategoryBinding.inflate(LayoutInflater.from(context),parent,false)
+
+        return HolderCategory(binding.root)
+    }
+
+    override fun getItemCount(): Int {
+        return categoryArrayList.size //Hiển thị số lượng item
+    }
+
+    override fun onBindViewHolder(holder: HolderCategory, position: Int) {
+        /*lất,thiết lập dữ liệu, xử lí sự kiện*/
+
+        //lay du lieu
+        val model = categoryArrayList[position]
+        val id = model.id
+        val category = model.category
+        val uid = model.uid
+        val timestamp = model.timestamp
+
+        //set data
+        holder.categoryTv.text = category
+
+        //xu li nut xoa
+        holder.deleteBtn.setOnClickListener {
+            //xác nhận trước khi xóa
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Xóa")
+                .setMessage("Xóa thể loại này?")
+                .setPositiveButton("Xóa"){a, d->
+                    Toast.makeText(context,"Đang xóa...",Toast.LENGTH_SHORT).show()
+                    deleteCategory(model,holder)
+                }
+                .setNegativeButton("Hủy"){a, d ->
+                    a.dismiss()
+
+                }
+        }
+    }
+
+    private fun deleteCategory(model: ModelCategory, holder: HolderCategory) {
+        //lấy id thể loại cần xóa
+        val id = model.id
+        val ref = FirebaseDatabase.getInstance().getReference("Category")
+        ref.child(id)
+            .removeValue()
+            .addOnSuccessListener {
+                Toast.makeText(context,"Xóa thành công!",Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {e ->
+                Toast.makeText(context,"Không thể xóa... Lỗi: ${e.message}",Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    inner class HolderCategory(itemView: View):RecyclerView.ViewHolder(itemView){
+        //khởi tạo giao diện người dùng
+        var categoryTv: TextView =  binding.categoryTv
+        var deleteBtn: ImageButton =  binding.deleteBtn
+    }
+
+
+}
---
+++ app/src/main/java/com/example/readify/ModelCategory.kt
@@ -0,0 +1,20 @@
+package com.example.readify
+
+class ModelCategory {
+
+    //các biến phải khớp trong firebase
+    var id:String = ""
+    var category:String = ""
+    var timestamp: Long = 0
+    var uid:String = ""
+
+    constructor()
+    constructor(id: String, category: String, timestamp: Long, uid: String) {
+        this.id = id
+        this.category = category
+        this.timestamp = timestamp
+        this.uid = uid
+    }
+
+
+}
